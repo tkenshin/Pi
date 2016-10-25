d2b9ff1f60a0a5f8b3e617d0051053354513163d
tkenshin <tsumurayakenshin@icloud.com>
2016-10-25 13:05:39 +0900

    init

---
+++ monte.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+void MonteCarlo(int n);
+
+int main(void) {
+
+	int a = 1000;
+
+	MonteCarlo(a);
+
+	return 0;
+
+}
+
+void MonteCarlo(int n) {
+	int i;
+	const double a = (sqrt(5) - 1) / 2;
+	double x, sum;
+
+	x = sum = 0;
+
+	for(i = 0; i < n; i++) {
+		if((x += a) >= 1){
+			x--;
+	
+		}
+		sum += sqrt(1 - x * x);
+
+	}
+	
+	printf("pi = %6.4f\n", 4 * sum / n);
+
+}
